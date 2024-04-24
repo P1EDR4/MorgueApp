66f6f87e599152e9cf32abf1fa283c0a8c083998
JesusALbert0 <paredesdzulja29@gmail.com>
2024-04-24 16:08:16 -0600

    si

--- src/Components/BarraMenu/sidebar.jsx
+++ src/Components/BarraMenu/sidebar.jsx
@@ -6,7 +6,7 @@ const Sidebar = () => {
   return (
     <div className="sidebar">
       <ul>
-        <li><Link to="/dashboard">Dashboard</Link></li>
+        <li><Link to="/Dashboard">Dashboard</Link></li>
         <li><Link to="/Tabla">Inicio</Link></li> 
         <li><Link to="/perfil">Perfil</Link></li>
       </ul>
