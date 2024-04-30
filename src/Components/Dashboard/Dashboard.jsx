e70bc97a04ecdc2890083f53bd3c852a2b928c1f
JesusALbert0 <paredesdzulja29@gmail.com>
2024-04-29 22:22:26 -0600

    columna nueva, actualizacion de moidificar

--- src/Components/Dashboard/Dashboard.jsx
+++ src/Components/Dashboard/Dashboard.jsx
@@ -1,12 +1,38 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import Clock from './Clock'; // Importa el componente Clock
 import './Dashboard.css';
 
 const Dashboard = () => {
+  const [totalInfo, setTotalInfo] = useState(0);
+  const [deletedInfo, setDeletedInfo] = useState(0);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get('https://api-morgueapp.onrender.com/info');
+        const data = response.data;
+        setTotalInfo(data.totalInfo);
+        setDeletedInfo(data.deletedInfo);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
-    <div className="dashboard-container"> 
+    <div className="dashboard-container">
       <h2>Bienvenido a Morgue App</h2>
+      <div>
+        <p>Total de información: {totalInfo}</p>
+        <p>Información eliminada: {deletedInfo}</p>
+      </div>
+      <Clock /> {/* Agrega el componente Clock aquí */}
     </div>
   );
 };
 
 export default Dashboard;
+
